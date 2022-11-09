04c2b19c2ebb8964ff738a16f93d4785a4014e64
Wenlin Huang <jackhwl@hotmail.com>
2022-11-09 08:03:47 -0500

    4. Specifying the Data Provider and Connection String

---
+++ EFCore2/SamuraiApp/SamuraiApp.Data/SamuraiContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SamuraiApp.Domain;
+using System.Configuration;
+
+namespace SamuraiApp.Data
+{
+	public class SamuraiContext:DbContext
+	{
+		public DbSet<Samurai> Samurais { get; set; }
+		public DbSet<Quote> Quotes { get; set; }
+		public DbSet<Battle> Battles { get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer(ConfigurationManager.ConnectionStrings["viDesktop.ConnectionString"].ToString());
+		}
+	}
+}
