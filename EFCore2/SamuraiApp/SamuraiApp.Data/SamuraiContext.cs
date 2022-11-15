1d3e3245d179055b6d253e8c716b3bb1107161c3
Wenlin Huang <jackhwl@hotmail.com>
2022-11-14 21:46:30 -0500

    1. Getting EF Core to Output SQL Logs

--- EFCore2/SamuraiApp/SamuraiApp.Data/SamuraiContext.cs
+++ EFCore2/SamuraiApp/SamuraiApp.Data/SamuraiContext.cs
@@ -1,10 +1,14 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SamuraiApp.Domain;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
 
 namespace SamuraiApp.Data
 {
 	public class SamuraiContext:DbContext
 	{
+		public static readonly LoggerFactory loggerFactory = (LoggerFactory)LoggerFactory.Create(builder => builder.AddConsole());
+
 		public DbSet<Samurai> Samurais { get; set; }
 		public DbSet<Quote> Quotes { get; set; }
 		public DbSet<Battle> Battles { get; set; }
@@ -12,7 +16,9 @@ namespace SamuraiApp.Data
 		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 		{
 			//ConfigurationManager.AppSettings["ConnectionString"]
-			optionsBuilder.UseSqlServer("Data Source=(localdb)\\mssqllocaldb; Initial Catalog=SamuraiAppData;Trusted_Connection=True;");
+			optionsBuilder
+				.UseLoggerFactory(loggerFactory)
+				.UseSqlServer("Data Source=(localdb)\\mssqllocaldb; Initial Catalog=SamuraiAppData;Trusted_Connection=True;");
 		}
 	}
 }
