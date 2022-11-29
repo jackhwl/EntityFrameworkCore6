f1714bc827e9174b20ae0964b32fc4b4bb344f22
Wenlin Huang <jackhwl@hotmail.com>
2022-11-29 10:46:11 -0500

    Add CustomerViewModel views

---
+++ MvcSalesApp/Controllers/CustomersWithOrdersController.cs
@@ -0,0 +1,58 @@
+﻿using MvcSalesApp.Models;
+using SalesModel.DomainClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcSalesApp.Controllers
+{
+    public class CustomersWithOrdersController : Controller
+    {
+        private OrderSystemContext db = new OrderSystemContext();
+
+        // GET: CustomersWithOrders
+        public ActionResult Index()
+        {
+            var custs = db.Customers.AsNoTracking()
+                .Select(c => new CustomerViewModel
+                {
+                    CustomerId = c.CustomerId,
+                    Name = c.FirstName + " " + c.LastName,
+                    OrderCount = c.Orders.Count()
+                })
+                .ToList();
+            return View(custs);
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var cust = db.Customers.AsNoTracking()
+                .Select(c => new CustomerViewModel
+                {
+                    CustomerId = c.CustomerId,
+                    Name = c.FirstName + " " + c.LastName,
+                    OrderCount = c.Orders.Count(),
+                    Orders = c.Orders.Select(
+                    o => new OrderViewModel
+                    {
+                        OrderSource = o.OrderSource,
+                        CustomerId = o.CustomerId,
+                        OrderDate = o.OrderDate
+                    }).ToList()
+                })
+                .FirstOrDefault(c => c.CustomerId == id);
+            if (cust == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cust);
+        }
+    }
+}
--- MvcSalesApp/ViewModels/OrderViewModel.cs
+++ MvcSalesApp/ViewModels/OrderViewModel.cs
@@ -5,7 +5,9 @@ namespace SalesModel.DomainClasses
 {
 	public class OrderViewModel
 	{
+		public int OrderId { get; set; }
 		public DateTime OrderDate { get; set; }
 		public OrderSource OrderSource { get; set; }
+		public int CustomerId { get; set; }
 	}
 }
