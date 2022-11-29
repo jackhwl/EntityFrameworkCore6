fed6ad9553dd58b0470ba8dd0c345c09d02c5050
Wenlin Huang <jackhwl@hotmail.com>
2022-11-28 21:57:09 -0500

    add two viewModels

---
+++ MvcSalesApp/ViewModels/CustomerViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SalesModel.DomainClasses
+{
+	public class CustomerViewModel
+	{
+		public CustomerViewModel()
+		{
+			Orders = new List<OrderViewModel>();
+		}
+		public int CustomerId { get; set; }
+		public string Name { get; set; }
+		public int OrderCount { get; set; }
+		public List<OrderViewModel> Orders { get; set; }
+	}
+}
---
+++ MvcSalesApp/ViewModels/OrderViewModel.cs
@@ -0,0 +1,11 @@
+﻿using SalesModel.DomainClasses.Enums;
+using System;
+
+namespace SalesModel.DomainClasses
+{
+	public class OrderViewModel
+	{
+		public DateTime OrderDate { get; set; }
+		public OrderSource OrderSource { get; set; }
+	}
+}
