2c1652deaf903e84536c1243439b397d7b2f1da8
Wenlin Huang <jackhwl@hotmail.com>
2022-12-03 19:04:37 -0500

    Add IModificationHistory

---
+++ NinjaModule2Demo/NinjaDomain.Classes/Clan.cs
@@ -0,0 +1,25 @@
+﻿using NinjaDomain.Classes.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NinjaDomain.Classes
+{
+	public class Clan : IModificationHistory
+	{
+		public Clan()
+		{
+			Ninjas = new List<Ninja>();
+		}
+		public int Id { get; set; }
+		public string ClanName { get; set; }
+		public List<Ninja> Ninjas { get; set; }
+
+		public DateTime DateModified { get; set; }
+		public DateTime DateCreated { get; set; }
+		public bool IsDirty { get; set; }
+	}
+
+}
