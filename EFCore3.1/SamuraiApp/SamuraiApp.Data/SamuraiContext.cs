da35b1d885642f213697bfd0da2be29e7fb8b701
Wenlin Huang <jackhwl@hotmail.com>
2022-12-10 09:36:45 -0500

    Creating the Data Model with EF Core

---
+++ EFCore3.1/SamuraiApp/SamuraiApp.Data/SamuraiContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SamuraiApp.Domain;
+using System;
+
+namespace SamuraiApp.Data
+{
+	public class SamuraiContext: DbContext
+	{
+		public DbSet<Samurai> Samurais { get; set; }
+		public DbSet<Quote> Quotes { get; set; }
+		public DbSet<Clan> Clans { get; set; }
+	}
+}
