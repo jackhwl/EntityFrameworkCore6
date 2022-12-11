e68c83e24214786f186910444c515ad8b83ac9c2
Wenlin Huang <jackhwl@hotmail.com>
2022-12-10 22:06:26 -0500

    add context

--- EFCore3.1/SamuraiApp/SamuraiApp.Data/SamuraiContext.cs
+++ EFCore3.1/SamuraiApp/SamuraiApp.Data/SamuraiContext.cs
@@ -9,5 +9,10 @@ namespace SamuraiApp.Data
 		public DbSet<Samurai> Samurais { get; set; }
 		public DbSet<Quote> Quotes { get; set; }
 		public DbSet<Clan> Clans { get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer("Data Source=(localdb)\\mssqllocaldb; Initial Catalog=SamuraiAppData31;Trusted_Connection=True;");
+		}
 	}
 }
