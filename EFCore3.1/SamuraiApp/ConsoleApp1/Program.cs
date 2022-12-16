4ebb81208a92dddf1aeddc5848719b7f9ac25f89
Wenlin Huang <jackhwl@hotmail.com>
2022-12-16 08:50:00 -0500

    Persisting Data in Disconnected Scenarios

--- EFCore3.1/SamuraiApp/ConsoleApp1/Program.cs
+++ EFCore3.1/SamuraiApp/ConsoleApp1/Program.cs
@@ -12,9 +12,11 @@ namespace ConsoleApp1
 
         private static void Main(string[] args)
         {
-            GetSamurais("Before Add:");
-            AddSamurai();
-            GetSamurais("After Add:");
+            //GetSamurais("Before Add:");
+            //AddSamurai();
+            //GetSamurais("After Add:");
+            //AddBattles();
+            QueryAndUpdateBattle_Disconnected();
             Console.Write("Press any key...");
             Console.ReadKey();
         }
@@ -25,7 +27,22 @@ namespace ConsoleApp1
             context.Samurais.Add(samurai);
             context.SaveChanges();
         }
-
+        private static void AddBattles()
+        {
+            var battle = new Battle { Name = "Battle of Okehazama", StartDate= new DateTime(1560, 5,1), EndDate = new DateTime(1560, 6, 15) };
+            context.Battles.Add(battle);
+            context.SaveChanges();
+        }
+        private static void QueryAndUpdateBattle_Disconnected()
+        {
+            var battle = context.Battles.AsNoTracking().FirstOrDefault();
+            battle.EndDate = new DateTime(1560, 6, 30);
+            using(var newContextInstance = new SamuraiContext())
+			{
+                newContextInstance.Battles.Update(battle);
+                newContextInstance.SaveChanges();
+			}
+        }
         private static void GetSamurais(string text)
         {
             var samurais = context.Samurais.Where(s => EF.Functions.Like(s.Name, "%abc%")).ToList();
