1d9de69ddf063c6e0f9607b4a7fd445843263ced
Wenlin Huang <jackhwl@hotmail.com>
2022-12-19 19:48:54 -0500

    A Sneak Peek at Writing and Reading Data

--- EFCore5/SamuriaApp/ReadMe.md
+++ EFCore5/SamuriaApp/ReadMe.md
@@ -1,3 +1,4 @@
 ## Section 2: Building Your First Application using EF Core
 * Setting up the Solution
 * Adding EF Core with the NuGet Package Manager
+* A Sneak Peek at Writing and Reading Data
--- EFCore5/SamuriaApp/SamuraiApp.Data/SamuraiContext.cs
+++ EFCore5/SamuriaApp/SamuraiApp.Data/SamuraiContext.cs
@@ -8,5 +8,10 @@ namespace SamuraiApp.Data
 	{
 		public DbSet<Samurai> Samurais { get; set; }
 		public DbSet<Quote>	Quotes { get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer("Data Source=(localdb)\\mssqllocaldb; Initial Catalog=SamuraiTestData5;Trusted_Connection=True;");
+		}
 	}
 }
