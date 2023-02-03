e2da94f86f0162ee3acad5eb004d378d9535449c
Wenlin Huang <jackhwl@hotmail.com>
2023-02-02 20:16:23 -0500

    The Domain Project Contains Entities

---
+++ EFCore6BestPractices/GloboTicket/GloboTicket.Domain/Entities/Venue.cs
@@ -0,0 +1,7 @@
+﻿namespace GloboTicket.Domain.Entities;
+public class Venue
+{
+    public int VenueId { get; set; }
+    public string Name { get; set; }
+    public string Address { get; set; }
+}
---
+++ EFCore6BestPractices/ReadMe.md
@@ -0,0 +1,2 @@
+## Section 2: Structuring Your Project for Cleanliness and Testability
+* The Domain Project Contains Entities
